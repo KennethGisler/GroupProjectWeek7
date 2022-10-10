fd42730d4324b6ba061c8a4ac033317c6fce624b
KennethGisler <112647331+KennethGisler@users.noreply.github.com>
2022-10-10 15:44:11 -0500

    one more lil thing

--- GroupProjectWeek7/src/model/ListDetails.java
+++ GroupProjectWeek7/src/model/ListDetails.java
@@ -3,17 +3,32 @@ package model;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
 /**
  * Kenneth Gisler - kgisler@dmacc.edu
  * CIS171 22149
  * Oct 10, 2022
  */
+@Entity
 public class ListDetails {
 	
+	@GeneratedValue
+	@Column(name="ID")
 	private int id;
 	private String listName; //we had this penned as "name", changing it to listName for a bit more specificity.
 	private LocalDate dateAdded; //the date that the list was added or built
+	@ManyToOne(cascade=CascadeType.PERSIST)
 	private Player player; //the player
+	@OneToMany(cascade=CascadeType.MERGE, fetch=FetchType.EAGER)
+	@JoinTable
 	private List<Unit> roster; //the army list player is useing or has.
 	
 	//private int totalPoints; Let's think about adding this cause it could be a nice touch. Tally up the total cost of the army that the player has.
